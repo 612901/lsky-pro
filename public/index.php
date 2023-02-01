693ed699c1ae6f3b05c851af167c6e1a45a3db88
wuying <106313976+612901@users.noreply.github.com>
2023-02-01 09:00:50 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,22 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+define('LARAVEL_START', microtime(false));
+
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
 /*
 |--------------------------------------------------------------------------
